c51892b8a73aa30b17c3a2d1948d36abc74d3a4f
Sapan Agrawal <sapanagrawal777@gmail.com>
2017-07-03 15:57:37 +0530

    updates

--- Project/Mesh Formation/Code/Binator.m
+++ Project/Mesh Formation/Code/Binator.m
@@ -2,11 +2,12 @@ function [v] = Binator(spacing,range,p)
 %% Getting Domain
 plot3(p(:,1),p(:,2),p(:,3),'*');
 figure
-s = spacing;
+s_xy = spacing(1);
+s_z = spacing(2);
 x_r = range(1);    
 y_r = range(2);
 z_r = range(3);
-[x,y,z] = meshgrid(0:s:x_r,0:s:y_r,0:s:z_r);
+[x,y,z] = meshgrid(0:s_xy:x_r,0:s_xy:y_r,0:s_xy:z_r);
 X = x(:);
 Y = y(:);
 Z = z(:);
@@ -16,8 +17,10 @@ P = [X,Y,Z];
 %% Getting Valid points
 v = zeros(m,n,o);
 C = intersect(P,p,'rows');
-C = uint64(C/s)
+C(:,1:2) = uint64(C(:,1:2)/s_xy);
+C(:,3) = uint64(C(:,3)/s_z);
 [r,c] = size(C);
+C = uint64(C);
 C = C + uint64(ones(r,c));
 for i = 1:r
     v(C(i,1),C(i,2),C(i,3))=1;
--- Project/Mesh Formation/test.m
+++ Project/Mesh Formation/test.m
@@ -2,8 +2,11 @@ close all; clear all; clc; format long e;
 points=linspace(-pi,pi,51);
 [x,y,z]=meshgrid(points,points,points);
 fun_function =cos(x)+cos(y)+cos(z);
-isosurface(x,y,z,fun_function,-1);
-isocaps(x,y,z,fun_function,-1);
+[f1,v1] = isosurface(x,y,z,fun_function,0);
+[f2,v2] = isocaps(x,y,z,fun_function,0);
+f3 = [ f1; f2+ length(v1(:,1)) ];
+v3 = [v1; v2];
+h = patch('faces',f3, 'vertices',v3);
 grid on;
 title(['My Fun Function, Contour Value = ',num2str(1.1)]);
 xlabel('X');ylabel('Y');zlabel('Z');
@@ -13,4 +16,4 @@ figure
 x = x(:);
 y = y(:);
 z = z(:);
-plot3(x,y,z,'*')
+svlcad('Cube.stl',f3,v3);
